69e6239168671863a2999b714eb2af59533d4f58
Neil <NeilMalcolm@users.noreply.github.com>
2021-05-31 20:21:19 +0100

    Add and fix tests

--- PokemonCardCatalogue.Tests/ServiceTests/TcgApiService/BasePokemonTcgApiServiceTests.cs
+++ PokemonCardCatalogue.Tests/ServiceTests/TcgApiService/BasePokemonTcgApiServiceTests.cs
@@ -1,12 +1,27 @@
 ﻿using Moq;
+using Moq.Protected;
+using PokemonCardCatalogue.Common;
 using PokemonCardCatalogue.Common.Context;
 using PokemonCardCatalogue.Common.Context.Interfaces;
+using PokemonCardCatalogue.Common.Models;
+using PokemonCardCatalogue.Common.Models.Data;
 using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace PokemonCardCatalogue.Tests.ServiceTests
 {
     public abstract class BasePokemonTcgApiServiceTests : BaseTestFixture
     {
+        protected const string FetchUrl = "http://127.0.0.1/Fetch";
+        protected const string GetUrl = "http://127.0.0.1/Get";
+        protected const string FetchNoCacheUrl = "http://127.0.0.1/FetchNoCache";
+        protected const string GetNoCacheUrl = "http://127.0.0.1/GetNoCache";
+
+        protected const string CacheCardName = "This is from the cache.";
+        protected const string HttpClientCardName = "This is from the web.";
+
         protected Mock<ICache> CacheMock { get; set; }
         protected Mock<HttpMessageHandler> HttpMessageHandlerMock { get; set; }
         protected PokemonTcgApiService TcgApiService { get; set; }
@@ -19,6 +34,89 @@ namespace PokemonCardCatalogue.Tests.ServiceTests
 
         protected override void SetupMocks()
         {
+            CacheMock.Setup(m => m.GetAsync<ApiResponseDataContainer<Card>>(It.IsIn(FetchUrl), It.IsAny<QueryParameters>()))
+                .ReturnsAsync
+                (
+                    new ApiResponseDataContainer<Card>
+                    {
+                        Data = new Card
+                        {
+                            Name = CacheCardName
+                        }
+                    }
+                );
+            
+            CacheMock.Setup(m => m.GetAsync<ApiListResponseDataContainer<Card>>(It.IsIn(GetUrl), It.IsAny<QueryParameters>()))
+                .ReturnsAsync
+                (
+                    new ApiListResponseDataContainer<Card>
+                    {
+                        Data = new Card[]
+                        {
+                            new Card
+                            {
+                                Name = HttpClientCardName
+                            }
+                        }
+                    }
+                );
+
+            HttpMessageHandlerMock.Protected()
+                .Setup<Task<HttpResponseMessage>>
+                (
+                    "SendAsync",
+                    ItExpr.Is<HttpRequestMessage>(m => m.RequestUri.ToString() == FetchNoCacheUrl),
+                    ItExpr.IsAny<CancellationToken>()
+                )
+                .ReturnsAsync
+                (
+                    new HttpResponseMessage
+                    {
+                        Content = new StringContent
+                        (
+                            JsonSerializer.Serialize
+                            (
+                                new ApiResponseDataContainer<Card>
+                                {
+                                    Data = new Card
+                                    {
+                                        Name = HttpClientCardName
+                                    }
+                                }
+                            )
+                        )
+                    }
+                );
+
+            HttpMessageHandlerMock.Protected()
+                .Setup<Task<HttpResponseMessage>>
+                (
+                    "SendAsync",
+                    ItExpr.Is<HttpRequestMessage>(m => m.RequestUri.ToString() == GetNoCacheUrl),
+                    ItExpr.IsAny<CancellationToken>()
+                )
+                .ReturnsAsync
+                (
+                    new HttpResponseMessage
+                    {
+                        Content = new StringContent
+                        (
+                            JsonSerializer.Serialize
+                            (
+                                new ApiListResponseDataContainer<Card>
+                                {
+                                    Data = new Card[]
+                                    {
+                                        new Card
+                                        {
+                                            Name = HttpClientCardName
+                                        }
+                                    }
+                                }
+                            )
+                        )
+                    }
+                );
 
         }
 
--- PokemonCardCatalogue.Tests/ServiceTests/TcgApiService/PokemonTcgApiServiceForceWebRequestTests.cs
+++ PokemonCardCatalogue.Tests/ServiceTests/TcgApiService/PokemonTcgApiServiceForceWebRequestTests.cs
@@ -4,7 +4,6 @@ using PokemonCardCatalogue.Common;
 using PokemonCardCatalogue.Common.Models;
 using PokemonCardCatalogue.Common.Models.Data;
 using System.Net.Http;
-using System.Text.Json;
 using System.Threading.Tasks;
 using Moq.Protected;
 using System.Threading;
@@ -14,69 +13,21 @@ namespace PokemonCardCatalogue.Tests.ServiceTests.TcgApiService
     [TestFixture]
     public class PokemonTcgApiServiceForceWebRequestTests : BasePokemonTcgApiServiceTests
     {
-        private const string ExampleUrl = "http://127.0.0.1/test";
-        private const string CacheCardName = "This is from the cache.";
-        private const string HttpClientCardName = "This is from the web.";
-        
-        protected override void SetupMocks()
-        {
-            base.SetupMocks();
-
-            CacheMock.Setup(m => m.GetAsync<ApiResponseDataContainer<Card>>(ExampleUrl, It.IsAny<QueryParameters>()))
-                .ReturnsAsync
-                (
-                    new ApiResponseDataContainer<Card>
-                    {
-                        Data = new Card
-                        {
-                            Name = CacheCardName
-                        }
-                    }
-                );
-
-            HttpMessageHandlerMock.Protected()
-                .Setup<Task<HttpResponseMessage>>
-                (
-                    "SendAsync",
-                    ItExpr.IsAny<HttpRequestMessage>(),
-                    ItExpr.IsAny<CancellationToken>()
-                )
-                .ReturnsAsync
-                (
-                    new HttpResponseMessage
-                    {
-                        Content = new StringContent
-                        (
-                            JsonSerializer.Serialize
-                            (
-                                new ApiResponseDataContainer<Card>
-                                {
-                                    Data = new Card
-                                    {
-                                        Name = HttpClientCardName
-                                    }
-                                }
-                            )
-                        )
-                    }
-                );
-        }
-
         [Test]
         public async Task WhenForceWebRequestIsTrue_ThenCacheIsNotAccessed()
         {
             var parameters = new QueryParameters();
-            var result = await TcgApiService.FetchAsync<Card>(ExampleUrl, parameters, true);
+            var result = await TcgApiService.FetchAsync<Card>(FetchNoCacheUrl, parameters, true);
 
             var card = result.Data;
             Assert.IsNotNull(card);
             Assert.AreEqual(HttpClientCardName, card.Name);
-            CacheMock.Verify(m => m.GetAsync<ApiResponseDataContainer<Card>>(ExampleUrl, parameters), Times.Never());
+            CacheMock.Verify(m => m.GetAsync<ApiResponseDataContainer<Card>>(FetchNoCacheUrl, parameters), Times.Never());
             HttpMessageHandlerMock.Protected().Verify
             (
                 "SendAsync",
                 Times.Once(),
-                ItExpr.Is<HttpRequestMessage>(m => m.RequestUri.ToString() == ExampleUrl),
+                ItExpr.Is<HttpRequestMessage>(m => m.RequestUri.ToString() == FetchNoCacheUrl),
                 ItExpr.IsAny<CancellationToken>()
             );
         }
@@ -85,17 +36,17 @@ namespace PokemonCardCatalogue.Tests.ServiceTests.TcgApiService
         public async Task WhenForceWebRequestIsFalse_ThenCacheIsAccessed()
         {
             var parameters = new QueryParameters();
-            var result = await TcgApiService.FetchAsync<Card>(ExampleUrl, parameters, false);
+            var result = await TcgApiService.FetchAsync<Card>(FetchUrl, parameters, false);
 
             var card = result.Data;
             Assert.IsNotNull(card);
             Assert.AreEqual(CacheCardName, card.Name);
-            CacheMock.Verify(m => m.GetAsync<ApiResponseDataContainer<Card>>(ExampleUrl, parameters), Times.Once());
+            CacheMock.Verify(m => m.GetAsync<ApiResponseDataContainer<Card>>(FetchUrl, parameters), Times.Once());
             HttpMessageHandlerMock.Protected().Verify
             (
                 "SendAsync", 
                 Times.Never(),
-                ItExpr.Is<HttpRequestMessage>(m => m.RequestUri.ToString() == ExampleUrl),
+                ItExpr.Is<HttpRequestMessage>(m => m.RequestUri.ToString() == FetchUrl),
                 ItExpr.IsAny<CancellationToken>()
             );
         }
